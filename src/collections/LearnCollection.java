26f62b697db19486d757d83b33f09a8e27500e92
Aawal92 <abdulawal3092@gmail.com>
2024-09-19 14:34:12 -0400

    Implemented list interface

--- src/collections/LearnCollection.java
+++ src/collections/LearnCollection.java
@@ -1,4 +1,26 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LearnCollection {
+
+    /*
+    LIST
+        - Ordered collections of data
+        - Able to hold duplicate value
+        - Preserves insertion order ( can access and insert by position)
+     */
+
+    public static void main(String[] args){
+        List<String> names = new ArrayList<>();
+        names.add("Tareq");
+        names.add("Dalal");
+        names.add("Abdullah");
+        names.add(2, "Tuhin");
+
+        for(String name : names){
+            System.out.println(name);
+        }
+    }
 }
